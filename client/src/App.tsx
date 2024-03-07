b9288e90fb961cc2ffa24e86f47b815ff143ee18
Iocko <silviilchev@gmail.com>
2024-03-07 22:56:12 +0200

    init electron

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+import './shared/styles/index.css'
+
+function App() {
+  return <div className="text-3xl font-bold underline">Hello!</div>
+}
+
+export default App
