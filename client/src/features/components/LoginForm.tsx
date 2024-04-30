9f4141f7f775730892f5162c68033355ab279ad8
Iocko <silviilchev@gmail.com>
2024-05-01 00:45:17 +0300

    Validate login form

--- client/src/features/components/LoginForm.tsx
+++ client/src/features/components/LoginForm.tsx
@@ -1,16 +1,38 @@
-import { Button, Column, Row } from 'shared/components'
+import { Button, Column, Form, Row } from 'shared/components'
+import { UserModel } from 'shared/types'
+import { z } from 'zod'
 
 import LoginInput from './LoginInput'
 
+const schema = z.object({
+  username: z.string(),
+  password: z.string()
+})
+
 const LoginForm = () => {
+  const onSubmit = () => {}
   return (
-    <Row className="w-full justify-center">
-      <Column className="w-[380px] items-center gap-10">
-        <LoginInput placeholder="Enter your username" />
-        <LoginInput placeholder="Enter your password" />
-        <Button className="ml-auto px-10 tablet:w-full">Login</Button>
-      </Column>
-    </Row>
+    <Form<UserModel> schema={schema} onSubmit={onSubmit}>
+      {(control) => (
+        <Row className="w-full justify-center">
+          <Column className="w-[380px] items-center gap-10">
+            <LoginInput
+              control={control}
+              type="text"
+              name="username"
+              placeholder="Enter your username"
+            />
+            <LoginInput
+              control={control}
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+            />
+            <Button className="tablet:w-full ml-auto px-10">Login</Button>
+          </Column>
+        </Row>
+      )}
+    </Form>
   )
 }
 export default LoginForm
